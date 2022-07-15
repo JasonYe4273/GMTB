ae9844b01ab8ad67f90abb88e5224ca36a0c984a
Jason Ye <jasonye4273@gmail.com>
2022-07-15 18:53:15 -0400

    Render grid

--- game.py
+++ game.py
@@ -6,28 +6,26 @@ from game_object import Object
 import pygame
 pygame.init()
 
-def render_grid(grid: Grid, screen: pygame.Surface) -> None:
+# Renders the grid and returns the unit length (half of a triangle side)
+def render_grid(grid: Grid, screen: pygame.Surface) -> float:
+    MARGIN = 200
+
     (screen_w, screen_h) = screen.get_size()
+    (adj_w, adj_h) = (screen_w - MARGIN, screen_h - MARGIN)
     (grid_x, grid_y) = grid.shape()
     x = 2*grid_x + grid_y
     y = math.sqrt(3)*grid_y
 
-    unit = min(screen_h / x, screen_w / y)
+    unit = min(adj_w / x, adj_h / y)
     grid_w = unit * x
     grid_h = unit * y
 
-    bl = ((screen_w - grid_w) / 2, (screen_h + grid_h) / 2)
+    print((adj_w, adj_h))
+    print((grid_x, grid_y))
+    print((x, y))
+    print((grid_w, grid_h))
 
-    # Draw diagonals
-    for i in range(grid_x+1):
-        x_offset = 2*i*unit
-        y_offset = grid_y*unit
-        pygame.draw.line(
-            screen,
-            (0,0,0),
-            (bl[0] + x_offset, bl[1]),
-            (bl[0] + x_offset + y_offset, bl[1] - math.sqrt(3)*y_offset)
-        )
+    bl = ((screen_w - grid_w) / 2, (screen_h + grid_h) / 2)
 
     # Draw horizontals
     for j in range(grid_y+1):
@@ -40,7 +38,40 @@ def render_grid(grid: Grid, screen: pygame.Surface) -> None:
             (bl[0] + y_offset + x_offset, bl[1] - math.sqrt(3)*y_offset)
         )
 
-    print("Rendered grid")
+    # Draw long diagonals
+    for i in range(grid_x+1):
+        x_offset = 2*i*unit
+        y_offset = grid_y*unit
+        pygame.draw.line(
+            screen,
+            (0,0,0),
+            (bl[0] + x_offset, bl[1]),
+            (bl[0] + x_offset + y_offset, bl[1] - math.sqrt(3)*y_offset)
+        )
+
+    # Draw first half of cross diagonals
+    for i in range(grid_x):
+        offset = i*unit
+        pygame.draw.line(
+            screen,
+            (0,0,0),
+            (bl[0] + 2*offset, bl[1]),
+            (bl[0] + offset, bl[1] - math.sqrt(3)*offset),
+        )
+
+    for i in range(grid_y):
+        br = (bl[0] + 2*grid_x*unit, bl[1])
+        tl = (bl[0] + grid_y*unit, bl[1] - math.sqrt(3)*grid_y*unit)
+
+        offset = i*unit
+        pygame.draw.line(
+            screen,
+            (0,0,0),
+            (br[0] + offset, br[1] - math.sqrt(3)*offset),
+            (tl[0] + 2*offset, tl[1]),
+        )
+
+    return unit
 
 
 # Set up the drawing window
@@ -48,7 +79,7 @@ screen = pygame.display.set_mode([1600, 900])
 
 # Run until the user asks to quit
 running = True
-grid = Grid(10,10)
+grid = Grid(5,5)
 while running:
     # Did the user click the window close button?
     for event in pygame.event.get():
@@ -57,10 +88,10 @@ while running:
 
     # Fill the background with white
     screen.fill((255, 255, 255))
-    print("Filled screen with white")
 
-    render_grid(grid, screen)
+    unit = render_grid(grid, screen)
 
+    # Display the screen
     pygame.display.flip()
 
 # Done! Time to quit.
